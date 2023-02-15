49b2b48ea7379f7f4c68654ca187e3127d088451
Leonardo Moraes <leonardomoraes1504@outlook.com>
2023-02-15 15:24:07 -0300

    alteration github pages

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import * as Components from './styles'
 import { Item } from '../../types/Item';
 
@@ -11,12 +10,8 @@ type Props = {
     onDelete:(id: number) => void;
     
 }
-
-
 export const ListItem = ({ item, onDelete }: Props) => {
    
-    
-
     const [isChecked, setIsChecked] = useState(item.done);
     
     return (
@@ -25,8 +20,6 @@ export const ListItem = ({ item, onDelete }: Props) => {
             type="checkbox"
             checked={isChecked}
             onChange={e =>  setIsChecked(e.target.checked)}
-            
-            
             />
             <label>{item.name}</label>
             <Components.DeleteButton onClick={() => onDelete(item.id)}><FontAwesomeIcon icon={faTrash} /></Components.DeleteButton>
